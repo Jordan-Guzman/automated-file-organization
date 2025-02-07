807e1ff6b12fd7027e5ee5dddb9b774d0f6d3049
Jordan-Guzman <jordanhguz@gmail.com>
2025-02-07 14:13:29 -0800

    Added a small GUI using tkinter just to make sure it works.

---
+++ file_organization.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import messagebox, filedialog
+
+# Dictionary mapping file extensions to associated directory
+directories = {
+    "Images": [".jpg", ".jpeg", ".png"],
+    "Text Files": [".txt"],
+    "JSON Files": [".json"],
+    "CSV Files": [".csv"],
+    "SQL Files": [".sql"],
+    "XML Files": [".xml"],
+    "Excel Files": [".xlsx"]
+}
+
+# Create the directories using keys from the directories dictionary
+
+
+# Scan the files and move them to the associated directory
+
+
+# GUI built with tkinter for user
+def tk_gui():
+    root = tk.Tk()
+    root.title("File Organizer")
+    root.geometry("400x300")
+    root.mainloop()
+
+# tk_gui()
