9fd3d77884394af97b16b4c202d51974f205810b
Jordan-Guzman <jordanhguz@gmail.com>
2025-02-09 14:28:22 -0800

    Creating logic for the creation of the directories

--- file_organization.py
+++ file_organization.py
@@ -15,9 +15,16 @@ directories = {
 }
 
 home = os.environ['HOME']
-
 # Create the directories using keys from the directories dictionary
 
+def handle_choice(choice):
+    match choice:
+        case ".jpg":
+            print("Create Images directory AND move file into the directory") # I'll need to handle the directory already existing
+        case _:
+            print("Error")
+
+handle_choice(".jpg")
 
 # Scan the files and move them to the associated directory
 
@@ -30,3 +37,17 @@ def tk_gui():
     root.mainloop()
 
 # tk_gui()
+
+def change_directory(path):
+    return os.chdir(path)
+
+change_directory(home)
+
+
+"""
+First step is to scan the files in a directory
+Keep track of the extensions (maybe save them in a set to scan set for creating directories)
+Create directories
+Scan files again to move them into their proper directory
+"""
+ 
